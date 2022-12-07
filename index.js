71848ff11e83517fe1c165b0b52884e16fb4e25e
Edmar Radanovis <edradanovis@gmail.com>
2022-12-07 10:24:23 -0300

    first update for test

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({message: "server is up Ed!!!"});
+  return response.json({message: "server is up Ed!BUNITÃO!!"});
 });
 
+app.get("/atualizou", (request, response) => {
+  return response.json({message: "server is up Ed!BUNITÃO!! e GOSTOSÃO!!!!!"});
+});
+
+
+app.post('/test',(request, response) => {
+  const {name, date} = request.body
+
+  return response.json({name, date})
+})
 app.listen(3333);
